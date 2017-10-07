bb0b67d11e3d64974b742f8f9e3090a1e691ac9b
Yentl Verhelst <yverhe01@tui.be>
2017-10-07 11:03:19 +0200

    Add Console Host App

---
+++ MyCalculator.Host.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MyCalculator.Application.Services;
+
+namespace MyCalculator.Host.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var calc = new MyCalculatorService();
+            var nr1 = 3;
+            var nr2 = 6;
+
+            var sum = calc.Add(nr1, nr2);
+
+            Console.WriteLine($"{nr1} + {nr2} = {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
