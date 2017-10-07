d75fe1a6908f6a712c1b6d435dbd14f57df6d096
Yentl Verhelst <yverhe01@tui.be>
2017-10-07 10:44:54 +0200

    Add TestSupport project with Given When Then Test

---
+++ TestSupport/Given_When_Then_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestSupport
+{
+    public abstract class Given_When_Then_Test
+        : IDisposable
+    {
+        protected Given_When_Then_Test()
+        {
+            Setup();
+        }
+
+        private void Setup()
+        {
+            Given();
+            When();
+        }
+
+        protected abstract void Given();
+
+        protected abstract void When();
+
+        public void Dispose()
+        {
+            Cleanup();
+        }
+
+        protected virtual void Cleanup()
+        {
+        }
+    }
+}
