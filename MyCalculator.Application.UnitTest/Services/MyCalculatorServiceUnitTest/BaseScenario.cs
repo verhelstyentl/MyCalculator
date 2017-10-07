4e476f56904a2a52e1b2cc346904ffe646bb0c3b
Yentl Verhelst <yverhe01@tui.be>
2017-10-07 10:51:50 +0200

    Add BaseScenario for MyCalculatorApplicationService UnitTest

---
+++ MyCalculator.Application.UnitTest/Services/MyCalculatorServiceUnitTest/BaseScenario.cs
@@ -0,0 +1,19 @@
+﻿using MyCalculator.Application.Services;
+using TestSupport;
+
+namespace MyCalculator.Application.UnitTest.Services.MyCalculatorServiceUnitTest
+{
+    public static class BaseScenario
+    {
+        public abstract class Given_Valid_Dependencies_And_Constructed_Instance_Scenario
+            : Given_When_Then_Test
+        {
+            protected MyCalculatorService Sut;
+
+            protected override void Given()
+            {
+                Sut = new MyCalculatorService();
+            }
+        }
+    }
+}
